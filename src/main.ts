95ba2955c7b7741a7de697db6d0bfbabe00c6e38
kaveeshanethruwan <kaveesha.james151@gmail.com>
2026-02-01 11:54:22 +0530

    feat: add sentry for error reporting

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import './instrument';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Global level
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
